b749f2c7577aafc07d47ad31a4232f47c022cbcf
wy471x <1004963485@qq.com>
2020-03-25 23:50:33 +0800

    chapter4 studying...

--- chapter4/exercise/exercise_4.4.c
+++ chapter4/exercise/exercise_4.4.c
@@ -5,12 +5,3 @@ long rsum(long *start, long count)
         return 0;
     return *start + rsum(start+1, count-1);
 }
-
-int main(void)
-{
-    long array[4] = {1,2,3,4};
-    long count = 3;
-    printf("sum=%ld\n", rsum(array, count));
-    return 0;
-}
-
--- chapter4/exercise/exercise_4.4.s
+++ chapter4/exercise/exercise_4.4.s
@@ -24,45 +24,5 @@ rsum:
 	.cfi_endproc
 .LFE75:
 	.size	rsum, .-rsum
-	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC0:
-	.string	"sum=%ld\n"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB76:
-	.cfi_startproc
-	subq	$56, %rsp
-	.cfi_def_cfa_offset 64
-	movq	%fs:40, %rax
-	movq	%rax, 40(%rsp)
-	xorl	%eax, %eax
-	movq	$1, (%rsp)
-	movq	$2, 8(%rsp)
-	movq	$3, 16(%rsp)
-	movq	$4, 24(%rsp)
-	movq	%rsp, %rdi
-	movl	$3, %esi
-	call	rsum
-	movq	%rax, %rdx
-	leaq	.LC0(%rip), %rsi
-	movl	$1, %edi
-	movl	$0, %eax
-	call	__printf_chk@PLT
-	movq	40(%rsp), %rcx
-	xorq	%fs:40, %rcx
-	jne	.L12
-	movl	$0, %eax
-	addq	$56, %rsp
-	.cfi_remember_state
-	.cfi_def_cfa_offset 8
-	ret
-.L12:
-	.cfi_restore_state
-	call	__stack_chk_fail@PLT
-	.cfi_endproc
-.LFE76:
-	.size	main, .-main
 	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
