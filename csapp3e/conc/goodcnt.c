13d2668ca7cf2835b55e2780bf13a4cb932416fc
wy471x <wy471x@qq.com>
2020-06-09 22:58:56 +0800

    chapter 12 studying...

--- csapp3e/conc/goodcnt.c
+++ csapp3e/conc/goodcnt.c
@@ -8,8 +8,8 @@ void *thread(void *vargp); /* Thread routine prototype */
 
 /* Global shared variables */
 /* $begin goodcntsemdef */
-    volatile long cnt = 0; /* Counter */
-    sem_t mutex;           /* Semaphore that protects counter */
+volatile long cnt = 0; /* Counter */
+sem_t mutex;           /* Semaphore that protects counter */
 /* $end goodcntsemdef */
 
 int main(int argc, char **argv) 
@@ -19,15 +19,15 @@ int main(int argc, char **argv)
 
     /* Check input argument */
     if (argc != 2) {
-	printf("usage: %s <niters>\n", argv[0]);
-	exit(0);
+        printf("usage: %s <niters>\n", argv[0]);
+        exit(0);
     }
     niters = atoi(argv[1]);
 
     /* Create threads and wait for them to finish */
-/* $begin goodcntseminit */
+    /* $begin goodcntseminit */
     Sem_init(&mutex, 0, 1);  /* mutex = 1 */
-/* $end goodcntseminit */
+    /* $end goodcntseminit */
     Pthread_create(&tid1, NULL, thread, &niters);
     Pthread_create(&tid2, NULL, thread, &niters);
     Pthread_join(tid1, NULL);
@@ -35,9 +35,9 @@ int main(int argc, char **argv)
 
     /* Check result */
     if (cnt != (2 * niters))
-	printf("BOOM! cnt=%ld\n", cnt);
+        printf("BOOM! cnt=%ld\n", cnt);
     else
-	printf("OK cnt=%ld\n", cnt);
+        printf("OK cnt=%ld\n", cnt);
     exit(0);
 }
 
@@ -46,13 +46,13 @@ void *thread(void *vargp)
 {
     int i, niters = *((int *)vargp);
 
-/* $begin goodcntthread */
+    /* $begin goodcntthread */
     for (i = 0; i < niters; i++) {
-	P(&mutex);
-	cnt++;
-	V(&mutex);
+        P(&mutex);
+        cnt++;
+        V(&mutex);
     }
-/* $end goodcntthread */
+    /* $end goodcntthread */
     return NULL;
 }
 /* $end goodcnt */
